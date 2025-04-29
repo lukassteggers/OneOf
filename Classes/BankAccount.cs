8cd918f1ea08f6e59f8fbea955acf380e8065393
Lukas Steggers <lukas.steggers@traperto.com>
2025-04-29 12:43:57 +0200

    WIP: - Anpassung gitignore - Erweiterung Beispielcode - Anlage Powerpoint-Präsentation

--- Classes/BankAccount.cs
+++ Classes/BankAccount.cs
@@ -1,21 +1,25 @@
 using OneOfExample.Exceptions;
 using OneOf;
+using OneOf.Types;
+
 namespace OneOfExample.Classes;
 
 public class BankAccount
 {
     public Guid Id { get; init; }
+    public int Pin { get; private set; }
     public string Name { get; private set; }
     public double Balance {get; private set;}
 
-    public BankAccount(string name, double balance)
+    public BankAccount(string name, int pin, double balance)
     {
         Id = new Guid();
+        Pin = pin;
         Name = name;
         Balance = balance;
     }
 
-    public BankAccount(string name) : this(name, 0.0)
+    public BankAccount(string name, int pin) : this(name, pin, 0.0)
     {
     }
 
@@ -31,19 +35,22 @@ public class BankAccount
         return Balance;
     }
 
-    public OneOf<double, ZeroAmountException> Deposit(OneOf<double, int, string> amount)
+    public OneOf<Success, ZeroAmountException> Deposit(OneOf<double, int, string> amount)
     {
+        // Match for returns
         double convertedAmount = amount.Match(
-            amountAsDouble => amountAsDouble,
-            amountAsInt => (double) amountAsInt,
-            amountAsString => Convert.ToDouble(amountAsString)
+            amountAsDouble => Balance + amountAsDouble,
+            amountAsInt => Balance + (double) amountAsInt,
+            amountAsString => Balance + Convert.ToDouble(amountAsString)
         );
 
         if (convertedAmount == 0)
         {
             return new ZeroAmountException("Amount cannot be zero");
         }
-        return Balance + convertedAmount;
+        
+        // returning OneOf.Types-Structs (Yes, No, Success
+        return new Success();
     }
     
     public OneOf<double, ZeroAmountException, PaymentFailureException> Withdraw(double amount)
@@ -60,5 +67,21 @@ public class BankAccount
         
         Balance -= amount;
         return Balance;
+    }
+
+    public OneOf<Success, PaymentFailureException> Withdrawal(OneOf<double, int, string> amount)
+    {
+        //Switch-Method (not returning any value)
+        amount.Switch(
+            amountAsDouble => Balance += amountAsDouble,
+            amountAsInt => Balance += (double) amountAsInt,
+            amountAsString =>
+            {
+                var convertedAmount = Convert.ToDouble(amountAsString);
+                if (convertedAmount == null)
+                {
+                    return new
+                }
+            });
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using OneOfExample.Classes;
 
-var businessAccount = new BankAccount("business", 100000.00);
+var businessAccount = new BankAccount("business", 1234, 100000.00);
 
 var result1 = businessAccount.Deposit("1,00");
 
-double newBalance = result1.Match(balance => balance,
+OneOf.Types.Yes newBalance = result1.Match(balance => balance,
     zeroAmountException => throw zeroAmountException
 );
 
-Console.WriteLine($"Balance: {newBalance}");
+Console.WriteLine($"Balance updated: {newBalance}");
